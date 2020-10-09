4218f82b65e605ea51139886fed81e1421e3cc1b
Nick Cipollo <nick.cipollo@grubhub.com>
2020-10-09 15:00:24 -0400

    Initial commit

---
+++ reactive-switft-toy/reactive_switft_toyApp.swift
@@ -0,0 +1,17 @@
+//
+//  reactive_switft_toyApp.swift
+//  reactive-switft-toy
+//
+//  Created by Nick Cipollo on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct reactive_switft_toyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
